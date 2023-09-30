957c484771f0390ea174982fefb12724f4f01d0b
bianca <bianca@Ubuntu3.0.myguest.virtualbox.org>
2023-09-29 21:02:34 -0300

    comentarios adicionados em lab4_q2

--- Lab4/lab4_q2.asm
+++ Lab4/lab4_q2.asm
@@ -1,9 +1,9 @@
 .data
-	A: .float	0.11 0.34 1.23 5.34 0.76 0.65 0.34 0.12 0.87 0.56
-	B: .float	7.89 6.87 9.89 7.12 6.23 8.76 8.21 7.32 7.32 8.22
-	result_A: .asciiz  "Media de A: "
-	result_B: .asciiz  "\nMedia de B: "
-	digite:	  .asciiz  "Digite o tamanho dos vetores A e B: "
+	A: 	  .float	0.11 0.34 1.23 5.34 0.76 0.65 0.34 0.12 0.87 0.56
+	B: 	  .float	7.89 6.87 9.89 7.12 6.23 8.76 8.21 7.32 7.32 8.22
+	result_A: .asciiz 	"Media de A: "
+	result_B: .asciiz  	"\nMedia de B: "
+	digite:	  .asciiz  	"Digite o tamanho dos vetores A e B: "
 
 .text
 
@@ -12,7 +12,7 @@
 	
 	li $v0, 4 		# Comando para escrever string
 	la $a0, digite 		# Carrega string (endereço)
-	syscall
+	syscall			# Escreve "Digite o tamanho dos vetores A e B: " no console
 	
 	# lendo N do teclado
 	li	$v0, 5		# Comando para ler inteiro
@@ -20,43 +20,46 @@
 	move	$s0, $v0	# $s0 = N
 	
 	# calculando média de A
-	move	$a0, $s0
-	la	$a1, A		# $a1 = end. A
+	move	$a0, $s0	# argumento $a0 = N
+	la	$a1, A		# argumento $a1 = endereço do vetor A
 	jal	media
 	
-	mtc1	$v0, $f12	
+	# movendo o retorno do procedimento media para $f12 --> para depois escrever no console esse valor
+	mtc1	$v0, $f12	# $f12 = média de A
 	
-	li 	$v0, 4		# Comando.
-	la 	$a0, result_A	# Carrega string (endereço).
-	syscall
+	li 	$v0, 4		# Comando para escrever string
+	la 	$a0, result_A	# Carrega string (endereço)
+	syscall			# Escreve "Media de A: " no console
 	
-	li	$v0, 2
-	syscall			# imprime o a média de A
+	li	$v0, 2		# Comando para escrever float
+	syscall			# Escreve a média de A no console
 	
 	# Calculando média de B
-	move	$a0, $s0
-	la	$a1, B		# $a1 = end. B
+	move	$a0, $s0	# argumento $a0 = N
+	la	$a1, B		# argumento $a1 = endereço do vetor B
 	jal	media
 	
+	# movendo o retorno do procedimento media para $f12 --> para depois escrever no console esse valor
 	mtc1	$v0, $f12	
 	
-	li 	$v0, 4		# Comando.
-	la 	$a0, result_B	# Carrega string (endereço).
-	syscall
+	li 	$v0, 4		# Comando para escrever string
+	la 	$a0, result_B	# Carrega string (endereço)
+	syscall			# Escreve "\nMedia de B: " no console
 	
-	li	$v0, 2
-	syscall	
+	li	$v0, 2		# Comando para escrever float
+	syscall			# Escreve a média de B no console
 	
-	li	$v0, 10		# comando para encerrar o programa
-	syscall			# encerra o programa
+	li	$v0, 10		# Comando para encerrar o programa
+	syscall			# Encerra o programa
+
 
 media:
 	
-	mtc1	$zero, $f0   	# f0 = somatorio
-	cvt.s.w	$f0, $f0  
+	mtc1	$zero, $f0   	# $f0 = somatorio
+	cvt.s.w	$f0, $f0  	# converte $f0 para float
 	
-	mtc1	$a0, $f1   	# f1 = N
-	cvt.s.w	$f1, $f1
+	mtc1	$a0, $f1   	# $f1 = N
+	cvt.s.w	$f1, $f1	# converte $f1 para float
 	
 	mul	$t7, $a0, 4
 	add	$t7, $t7, $a1	# $t7 recebe endereço final do vetor + 1 ( já que começa em zero, esse número está 1 posição a mais)
@@ -67,7 +70,7 @@ media:
 		addi	$a1, $a1, 4	# endereço do vetor += 4
 		bne	$a1, $t7, Loop 	# quando endereço do vetor chegar no último + 1 ( já q começa em zero), loop para
 	
-	div.s	$f0, $f0, $f1
-	mfc1	$v0, $f0
+	div.s	$f0, $f0, $f1	# $f0 = $f0 / $f1   -->   resultado = somatório / N
+	mfc1	$v0, $f0	# retorno $v0 = resultado
 	jr	$ra
 
