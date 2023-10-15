271e1805a5d754461f261c191fb4bc50716fb4dd
bianca <bianca@Ubuntu3.0.myguest.virtualbox.org>
2023-10-15 16:29:13 -0300

    parte 1 certa

--- ed/projeto1/main.cpp
+++ ed/projeto1/main.cpp
@@ -20,31 +20,44 @@ int main() {
     if (myfile.is_open()) {
     	while (getline(myfile, line)) {
     		for (int i = 0; i < line.size(); i++) {
+    			//std::cout << "select: " << select << std::endl;
     			if (line[i] == '<') {
     				select = 1;
     				palavra = "";
+    				//std::cout << "abre <: " << palavra << std::endl;
     			} else if (line[i] == '>') {
     				if (select == 2) 
     					select = 4;
     				else if (select == 3)
     					select = 5;
-    			}
-    			if (select = 1) {
-    				if (line[i] == '/') 
+    				//std::cout << "fecha >: " << palavra << std::endl;
+    			} else if (select == 1) {
+    				if (line[i] == '/') {
     					select = 2;
-    				else 
+    					//std::cout << "com barra: " << palavra << std::endl;
+    				} else {
+    					palavra += line[i];
     					select = 3;
+    					//std::cout << "sem barra: " << palavra << std::endl;
+    				}
     			} else if (select == 2 || select == 3) {
     				palavra += line[i];
+    				//std::cout << "add: " << palavra << std::endl;
     			}
     			if (select == 5) {
     				pilha->push(palavra);
+    				//std::cout << "palavra empilhada: " << palavra << std::endl;
+    				//std::cout << "top: " << pilha->top() << std::endl;
+    				select = 0;
     			} else if (select == 4) {
     				if (pilha->top() == palavra) {
     					pilha->pop();
+    					//std::cout << "pop: " << palavra << std::endl;
     				} else {
     					std::cout << "erro" << std::endl;
+    					return 0;
     				}
+    				select = 0;
     			}
     				
     		}
@@ -54,6 +67,7 @@ int main() {
     	myfile.close();
     	if (!pilha->empty()) {
     		std::cout << "erro" << std::endl;
+    		return 0;
     	}
     	
     } else std::cout << "Erro ao abrir o arquivo" << std::endl;
@@ -62,4 +76,3 @@ int main() {
 
     return 0;
 }
-
