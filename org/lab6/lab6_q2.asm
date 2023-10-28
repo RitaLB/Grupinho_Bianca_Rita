282eb87fc7f568e9f5652f15c45753c80076bdf1
biancaverzola <120264811+biancaverzola@users.noreply.github.com>
2023-10-28 19:10:34 -0300

    comentarios adicionados

--- org/lab6/lab6_q2.asm
+++ org/lab6/lab6_q2.asm
@@ -7,45 +7,31 @@
 	li	$v0, 4		# Comando para escrever string no console
 	la	$a0, digite	# "Digite o valor a ser calculado o fatorial: " será escrito
 	syscall			
-
+	
+	#   --- início calculo do fatorial ---
 	
 	li	$v0, 5		# Comando para ler inteiro do teclado
 	syscall
+	move	$t0, $v0	# $t0 = valor lido
 	
-	move	$a0, $v0	# $t0 = valor lido
-	jal	fatorial
-	move	$s5, $v0
+	li	$s0, 1		# $s0 será o resultado do fatorial, inicializa com 1
+
+  loop:
+	slti	$t1, $t0, 2		# $t1 = 1 se $t0 < 2
+	bne	$t1, $zero, exit	# Sai do loop se $t1 != 0, ou seja, se $t1 < 2
 	
-	li	$v0, 4		# Comando para escrever string no console
+	mul	$s0, $s0, $t0		# $s0 = $s0 * $t0
+	subi	$t0, $t0, 1		# Decrementa $t0
+	
+	j	loop			# Volta para o loop
+
+ exit:
+ 	#   --- fim cálculo do fatorial ---
+ 	
+ 	li	$v0, 4		# Comando para escrever string no console
  	la	$a0, resultado	# "Resultado: " será escrito
  	syscall
  	
   	li	$v0, 1		# Comando para escrever inteiro no console
-  	move 	$a0, $s5	# Valor a ser escrito no console: fatorial do número lido
+  	move 	$a0, $s0	# Valor a ser escrito no console: fatorial do número lido
   	syscall
-	
-	li	$v0, 10
-	syscall
-
-
-  fatorial:
-	slti	$t1, $a0, 2
-	beq	$t1, $zero, label1
-	
-	li	$v0, 1
-	jr	$ra
-	
-  label1:
-	addi	$sp, $sp, -8
-	sw	$a0, 4($sp)
-	sw	$ra, 0($sp)
-	
-	subi	$a0, $a0, 1
-	jal	fatorial
-	
-	lw	$ra, 0($sp)
-	lw	$a0, 4($sp)
-	addi	$sp, $sp, 8
-
-	mul	$v0, $v0, $a0
-	jr	$ra
